a6cdf4c0c2821b92f1644b9e7dd7679f6bfa8b0b
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-05-09 11:32:30 -0400

    commit tree 1a2af86909b9ef76748aea98937a0f689fd23564

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your code here
+
+class Dog{
+    constructor(name, breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+}
+
+const bigDog1 = new Dog ('mary', 'poodle')
+const bigDog2 = new Dog ('gary', 'doodle')
+
+class Breakfast{
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink
+    }
+}
+
+class Lunch{
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner{
+    #desert;
+    constructor(salad, soup, entry, desert) {
+        this.salad = salad;
+        this.soup = soup;
+        this.entry = entry;
+        this.#desert = desert;
+    }
+}
